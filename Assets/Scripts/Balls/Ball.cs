ab748b328b6cd6dd766e8a1e966bc497c26569a0
PowderDuck <veigarmainer14@gmail.com>
2025-07-27 18:45:31 +0000

    Cannon

--- Assets/Scripts/Balls/Ball.cs
+++ Assets/Scripts/Balls/Ball.cs
@@ -4,8 +4,23 @@ namespace Momentum.Scripts.Balls
 {
     public abstract class Ball : MonoBehaviour
     {
+        [field: SerializeField]
+        public Sprite Sprite { get; private set; }
+
         protected Vector2 _direction = Vector2.zero;
         public Vector2 Direction => _direction;
+
+        protected Rigidbody2D _rigidbody = default!;
+
+        protected void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            _rigidbody.linearVelocity = _direction;
+        }
 
         public void SetDirection(Vector2 direction)
         {
--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,9 +1,58 @@
+using System.Collections.Generic;
+using Momentum.Scripts.Balls;
 using UnityEngine;
 
 namespace Momentum.Scripts
 {
     public class Cannon : MonoBehaviour
     {
+        [SerializeField] private SpriteRenderer _spoilerRenderer = default!;
 
+        [SerializeField] private Transform _upperBoundary;
+        [SerializeField] private Transform _lowerBoundary;
+
+        [SerializeField] private List<Ball> _ballPrefabs = default!;
+
+        private Vector2 _cannonPosition = Vector2.zero;
+
+        private Ball _currentBall = default!;
+
+        private void Start()
+        {
+            _cannonPosition = transform.position;
+
+            NextBall(_ballPrefabs[Random.Range(0, _ballPrefabs.Count)]);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ball = Instantiate(
+                    _currentBall,
+                    transform.position,
+                    Quaternion.identity);
+                ball.SetDirection(transform.right);
+                NextBall(_ballPrefabs[Random.Range(0, _ballPrefabs.Count)]);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            _cannonPosition.Set(
+                _cannonPosition.x,
+                Mathf.Clamp(
+                    Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
+                    _lowerBoundary.position.y,
+                    _upperBoundary.position.y));
+
+            transform.position = _cannonPosition;
+        }
+
+        private void NextBall(Ball ball)
+        {
+            _currentBall = ball;
+            _spoilerRenderer.sprite = ball.Sprite;
+        }
     }
 }
